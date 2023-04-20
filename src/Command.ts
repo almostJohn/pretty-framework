1f57841674090156cde0d35d64b61c7747b33f75
Jhan Gale Garcia <garcia.johngale@gmail.com>
2023-04-20 18:49:32 +0800

    backwards compatible new features

--- src/Command.ts
+++ src/Command.ts
@@ -1,5 +1,6 @@
 import type { CommandPayload } from "./types/ArgumentsOf.js";
 import type { ArgsParam, InteractionParam, CommandMethod, Commands } from "./types/Interaction.js";
+import { logger } from "./logger.js";
 
 export abstract class Command<C extends CommandPayload = CommandPayload> implements Commands<C> {
 	public constructor(public readonly name?: C["name"][]) {}
@@ -8,21 +9,28 @@ export abstract class Command<C extends CommandPayload = CommandPayload> impleme
 		interaction: InteractionParam<CommandMethod.ChatInput>,
 		_args: ArgsParam<CommandMethod.ChatInput>,
 	): Promise<void> | void {
-		console.log(`Received chat input for ${interaction.commandName}, but the command does not handle chat input`);
+		logger.info(
+			{ command: { name: interaction.commandName, type: interaction.type }, userId: interaction.user.id },
+			`Received chat input for ${interaction.commandName}, but the command does not handle chat input`,
+		);
 	}
 
 	public autocomplete(
 		interaction: InteractionParam<CommandMethod.Autocomplete>,
 		_args: ArgsParam<CommandMethod.Autocomplete>,
 	): Promise<void> | void {
-		console.log(`Received autocomplete for ${interaction.commandName}, but the command does not handle autocomplete`);
+		logger.info(
+			{ command: { name: interaction.commandName, type: interaction.type }, userId: interaction.user.id },
+			`Received autocomplete for ${interaction.commandName}, but the command does not handle autocomplete`,
+		);
 	}
 
 	public messageContext(
 		interaction: InteractionParam<CommandMethod.MessageContext>,
 		_args: ArgsParam<CommandMethod.MessageContext>,
 	): Promise<void> | void {
-		console.log(
+		logger.info(
+			{ command: { name: interaction.commandName, type: interaction.type }, userId: interaction.user.id },
 			`Received message context for ${interaction.commandName}, but the command does not handle message context`,
 		);
 	}
@@ -31,6 +39,9 @@ export abstract class Command<C extends CommandPayload = CommandPayload> impleme
 		interaction: InteractionParam<CommandMethod.UserContext>,
 		_args: ArgsParam<CommandMethod.UserContext>,
 	): Promise<void> | void {
-		console.log(`Received user context for ${interaction.commandName}, but the command does not handle user context`);
+		logger.info(
+			{ command: { name: interaction.commandName, type: interaction.type }, userId: interaction.user.id },
+			`Received user context for ${interaction.commandName}, but the command does not handle user context`,
+		);
 	}
 }
