cee005df7d5eceaa1f21ae794335fa72bf64f791
Jhan Gale Garcia <garcia.johngale@gmail.com>
2023-04-22 12:44:16 +0800

    bug fixes and improvements

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 export * from "./components/index.js";
+
 export * from "./container/index.js";
+
 export * from "./types/index.js";
+
 export * from "./util/index.js";
 
 export * from "./Command.js";
-
 export * from "./logger.js";
 
 export { container } from "tsyringe";
