85a28d1fe9481cb7334dc0af7435c4a68064b580
Jhan Gale Garcia <garcia.johngale@gmail.com>
2023-04-23 20:11:29 +0800

    feat: locale for text translation

--- src/Command.ts
+++ src/Command.ts
@@ -1,11 +1,15 @@
 import type { CommandPayload } from "./types/ArgumentsOf.js";
-import type { ArgsParam, InteractionParam, CommandMethod, Commands } from "./types/Interaction.js";
+import type { ArgsParam, InteractionParam, LocaleParam, CommandMethod, Commands } from "./types/Interaction.js";
 import { logger } from "./logger.js";
 
 export abstract class Command<C extends CommandPayload = CommandPayload> implements Commands<C> {
 	public constructor(public readonly name?: C["name"][]) {}
 
-	public chatInput(interaction: InteractionParam<CommandMethod.ChatInput>, _args: ArgsParam<C>): Promise<void> | void {
+	public chatInput(
+		interaction: InteractionParam<CommandMethod.ChatInput>,
+		_args: ArgsParam<C>,
+		_locale: LocaleParam,
+	): Promise<void> | void {
 		logger.info(
 			{ command: { name: interaction.commandName, type: interaction.type }, userId: interaction.user.id },
 			`Received chat input for ${interaction.commandName}, but the command does not handle chat input`,
@@ -15,6 +19,7 @@ export abstract class Command<C extends CommandPayload = CommandPayload> impleme
 	public autocomplete(
 		interaction: InteractionParam<CommandMethod.Autocomplete>,
 		_args: ArgsParam<C>,
+		_locale: LocaleParam,
 	): Promise<void> | void {
 		logger.info(
 			{ command: { name: interaction.commandName, type: interaction.type }, userId: interaction.user.id },
@@ -25,6 +30,7 @@ export abstract class Command<C extends CommandPayload = CommandPayload> impleme
 	public messageContext(
 		interaction: InteractionParam<CommandMethod.MessageContext>,
 		_args: ArgsParam<C>,
+		_locale: LocaleParam,
 	): Promise<void> | void {
 		logger.info(
 			{ command: { name: interaction.commandName, type: interaction.type }, userId: interaction.user.id },
@@ -35,6 +41,7 @@ export abstract class Command<C extends CommandPayload = CommandPayload> impleme
 	public userContext(
 		interaction: InteractionParam<CommandMethod.UserContext>,
 		_args: ArgsParam<C>,
+		_locale: LocaleParam,
 	): Promise<void> | void {
 		logger.info(
 			{ command: { name: interaction.commandName, type: interaction.type }, userId: interaction.user.id },
--- src/types/Interaction.ts
+++ src/types/Interaction.ts
@@ -7,22 +7,35 @@ import type {
 import type { ArgumentsOf, CommandPayload } from "./ArgumentsOf.js";
 
 export type ChatInput<C extends CommandPayload> = {
-	chatInput(interaction: ChatInputCommandInteraction<"cached">, args: ArgumentsOf<C>): Promise<void> | void;
+	chatInput(
+		interaction: ChatInputCommandInteraction<"cached">,
+		args: ArgumentsOf<C>,
+		locale: string,
+	): Promise<void> | void;
 };
 
 export type Autocomplete<C extends CommandPayload> = {
-	autocomplete(interaction: AutocompleteInteraction<"cached">, args: ArgumentsOf<C>): Promise<void> | void;
+	autocomplete(
+		interaction: AutocompleteInteraction<"cached">,
+		args: ArgumentsOf<C>,
+		locale: string,
+	): Promise<void> | void;
 };
 
 export type MessageContext<C extends CommandPayload> = {
 	messageContext(
 		interaction: MessageContextMenuCommandInteraction<"cached">,
 		args: ArgumentsOf<C>,
+		locale: string,
 	): Promise<void> | void;
 };
 
 export type UserContext<C extends CommandPayload> = {
-	userContext(interaction: UserContextMenuCommandInteraction<"cached">, args: ArgumentsOf<C>): Promise<void> | void;
+	userContext(
+		interaction: UserContextMenuCommandInteraction<"cached">,
+		args: ArgumentsOf<C>,
+		locale: string,
+	): Promise<void> | void;
 };
 
 export type Commands<C extends CommandPayload> = Autocomplete<C> &
@@ -52,3 +65,8 @@ export type ArgsParam<
 	C extends CommandPayload,
 	M extends CommandMethod = CommandMethod.ChatInput,
 > = CommandMethodParameters<C, M>[1];
+
+export type LocaleParam<M extends CommandMethod = CommandMethod.ChatInput> = CommandMethodParameters<
+	CommandPayload,
+	M
+>[2];
