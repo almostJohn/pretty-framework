ae7db58df810613177882838fdcf954201225450
Jhan Gale Garcia <garcia.johngale@gmail.com>
2023-06-10 21:39:34 +0800

    add: removedUndefinedKeys function

--- src/util/index.ts
+++ src/util/index.ts
@@ -2,6 +2,7 @@ export * from "./dynamicImport.js";
 export * from "./ellipsis.js";
 export * from "./embed.js";
 export * from "./parseMessageLink.js";
+export * from "./removedUndefinedKeys.js";
 export * from "./resolveChannel.js";
 export * from "./resolveMemberAndUser.js";
 export * from "./truncate.js";
